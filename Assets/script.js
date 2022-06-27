f1dc1a29bc16a82ee51bfd96e84627165bd20a71
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-06-27 16:29:43 -0500

    wrote out list of questions

---
+++ Assets/script.js
@@ -0,0 +1,30 @@
+// use array of questions. each question is an objet 
+var questions = [
+    {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "alerts"
+    },
+{
+    title: "The condition in an if/else statement is enclosed with_____.",
+    choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    answer: "alerts" 
+}, 
+{
+    title: "Arrays in JavaScript can be used to store ______.",
+    choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "alerts"
+},
+{ 
+    title: "String values must be enclosed within _____ when being assigned to varaibles.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answer: "alerts"
+
+},
+{
+    title: "A very useful tool used during developement and debugging for printing content to the debugger is:",
+    choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "console.log"],
+    answer: "alerts"
+
+},
+]
